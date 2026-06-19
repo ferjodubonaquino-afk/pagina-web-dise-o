bcb1dfe8e1534476956964263ee8e990e1acc552
ferjodubonaquino-afk <ferjo.dubon.aquino@gmail.com>
2026-06-18 23:19:22 -0600

    Add files via upload

---
+++ mainpaginaweb.js
@@ -0,0 +1,30 @@
+window.onscroll = function(){
+
+    let boton = document.getElementById("btnArriba");
+
+    if(document.documentElement.scrollTop > 300){
+        boton.style.display = "block";
+    }else{
+        boton.style.display = "none";
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let boton = document.getElementById("btnArriba");
+
+    if(boton){
+
+        boton.addEventListener("click", function(){
+
+            window.scrollTo({
+                top:0,
+                behavior:"smooth"
+            });
+
+        });
+
+    }
+
+})
